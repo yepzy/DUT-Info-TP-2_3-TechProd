66167d4666b8cab3b881b44c965b2dd7645b1f99
Merlin BARZILAI <merlin.barzilai@gmail.com>
2016-03-23 10:07:01 +0100

    ajout de SqliteClass

--- Class/SqliteClass.py
+++ Class/SqliteClass.py
@@ -1,19 +1,43 @@
 import sqlite3
 import json
 import os
-with open("config.json") as config:
-    dataConfig = json.loads(config.read())
 
-dbPath = dataConfig['dbPath']
+class SqliteConn:
 
-def getDbAllDataFrom(tablename,value="*"):
-	try:
-		conn = sqlite3.connect(dbPath)
-		c = conn.cursor()
-		s = 'SELECT ? FROM ? ;'
-		tab = [value,tablename]
-		return c.execute(s,[value,tablename]).fetchall()
-	except sqlite3.Error as e:
-		print (e.args[0])
+    db = ''
+    conn = ''
 
- #def getDbSpeDataFrom(tablename,value="*",):
+    # the constructor
+    def __init__(filepath="*"):
+        if filepath=="*":
+            with open("config.json") as config:
+            dataConfig = json.loads(config.read())
+
+            db = dataConfig['dbPath']
+        else :
+            db = filepath
+        try:
+    		conn = sqlite3.connect(db)
+        except sqlite3.Error as e:
+    		print (e.args[0])
+
+    def query(_from, _select="*", _where="1==1"):
+        try:
+            c = conn.cursor()
+            req = 'SELECT ? FROM ? WHERE ?'
+            args = [_select, _from, _where]
+            return c.execute(req, args).fetchAll();
+        except sqlite3.Error as e:
+    		print (e.args[0])
+
+    def insert(_in, _values):
+        try:
+            c = conn.cursor()
+            req = 'INSERT INTO ? VALUES ?'
+            args = [_in, _values]
+            return c.execute(req, args).fetchAll();
+        except sqlite3.Error as e:
+    		print (e.args[0])
+
+
+    #def getDbSpeDataFrom(tablename,value="*",):
